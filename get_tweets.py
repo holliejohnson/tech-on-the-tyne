b5c9f562ee872a8f72a9cef46d6dbbaca20a0c51
holliejohnson <hollie.johnson@newcastle.ac.uk>
2023-02-20 16:15:28 +0000

    Get tweets

--- get_tweets.py
+++ get_tweets.py
@@ -2,21 +2,18 @@ import tweepy
 import pandas as pd
 
 # twitter api auth
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAALoqlQEAAAAA%2BlwMzw416xJpW866q4JfuvhujLE%3DgeyjfQYdRvaKNvjaODuQDOlCSOSp7yB1NU8LDnMOJNoHExoYTP'
-
+bearer_token = None
 client = tweepy.Client(bearer_token)
 
 query = '(elon musk) lang:en -is:retweet'
 
-response = client.search_recent_tweets(query = query, max_results=100)
 
 output = []
-for i in range(200):
-    response = client.search_recent_tweets(query = query, max_results=100)
-    for tweet in response.data:
-        text = tweet.text
-        line = {'text' : text}
-        output.append(line)
+response = client.search_recent_tweets(query = query, max_results=100)
+for tweet in response.data:
+    text = tweet.text
+    line = {'text' : text}
+    output.append(line)
 
 tweets_df = pd.DataFrame(output)
 tweets_df.to_csv('elon_musk_tweets.csv')
